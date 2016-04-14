ba081980110c633572e54a43c23f8b2c12ef59d6
clement <clement@la-cooperative-des-tilleuls.com>
2016-04-14 16:58:49 +0200

    entity mini blog

---
+++ src/AppBundle/Entity/Category.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * Category.
+ *
+ * @ORM\Entity
+ */
+class Category
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", nullable=false)
+     * @Assert\Type(type="string")
+     * @Groups({ "category_read", "category_write", "blog_posting_read", "blog_posting_write" })
+     */
+    private $name;
+
+    /**
+     * Set name.
+     *
+     * @param string $name
+     *
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
